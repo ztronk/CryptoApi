0fa32d80ad7bc8ba0160b712318552ca7b8fc934
79112 <79112@DESKTOP-MKHBCEQ>
2020-03-04 22:38:09 +0300

    Add project files.

---
+++ CryptoApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CryptoApi.Startup))]
+namespace CryptoApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
